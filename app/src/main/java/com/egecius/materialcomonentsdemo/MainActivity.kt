a0eec2a4176993152ed23dbbb9b2e27bd291178d
Egidijus Gecius <e.gecius@gmail.com>
2020-02-26 11:44:54 +0000

    show alert dialog

--- app/src/main/java/com/egecius/materialcomonentsdemo/MainActivity.kt
+++ app/src/main/java/com/egecius/materialcomonentsdemo/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.egecius.materialcomonentsdemo
 
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showAlertDialog(view: View) {
+        AlertDialog.Builder(this)
+            .setMessage("Do you want to take your action?")
+            .setPositiveButton("Ok", null)
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.cancel()
+            }
+            .show()
     }
 }

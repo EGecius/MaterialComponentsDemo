497bfede630451df729e2dd042b9b46561b9c32c
Egidijus Gecius <e.gecius@gmail.com>
2020-06-03 15:32:58 +0100

    add ToggleButtonActivity

---
+++ app/src/main/java/com/egecius/materialcomponentsdemo/ToggleButtonActivity.kt
@@ -0,0 +1,12 @@
+package com.egecius.materialcomponentsdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ToggleButtonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toggle_button)
+    }
+}
